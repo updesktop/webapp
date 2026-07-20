40a76ec2eab77bea3af5b512e1645696fe71a244
UPDESKTOP Digital <updesktop@gmail.com>
2026-07-20 10:47:54 +0800

    Add files via upload

---
+++ radzspeed/sw_radz.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'jwebapps-v22';
+const ASSETS = [
+  './',
+  '.index.html',
+  '../zzz_main/app.js','../zzz_main/lib.js',
+  './site.webmanifest',
+  './enadsys.json',
+  './logo.png',
+  './qrcode.png',
+  './mainpic.png'
+];
+
+// Install and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(err => console.log('Cache error:', err))
+  );
+});
+
+// Remove old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Serve from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => cachedResponse || fetch(event.request))
+  );
+});
